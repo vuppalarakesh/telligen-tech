5034d8838991ee1f1a38d822a7ae35db7f438fd0
rakesh vuppala <rvuppala@liaison-intl.com>
2016-07-20 22:52:01 -0400

    server js file

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+
+/*app.get('/', function (req, res) {
+ res.send('Hello World!');
+ });*/
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!');
+});
